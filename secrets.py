6e63f6c5c582d58c3b3217065466d6f13e31e99b
Max Renaud <maxrd@google.com>
2021-12-01 12:33:51 -0800

    Initial commit

---
+++ secrets.py
@@ -0,0 +1,12 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'Your_SSID',
+    'password' : 'SSID_Password',
+    'aio_username' : 'my_adafruit_io_username',
+    'aio_key' : 'my_adafruit_io_key',
+    'timezone' : "America/Los_Angeles", # http://worldtimeapi.org/timezones
+    'openweather_token': "The Token",
+    'openweather_location': "Sunnyvale, US",
+    }
